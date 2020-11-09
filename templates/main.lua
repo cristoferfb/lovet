dc04d31a6b1bce0e38ec74f30da5db366114520f
Cristofer Fuentes <contacto@cristoferfb.com>
2020-11-08 23:02:02 -0300

    improve example code in main.lua

--- templates/main.lua
+++ templates/main.lua
@@ -1,17 +1,15 @@
 function love.load()
     -- preload some stuffs
     lovet = love.graphics.newImage("assets/images/lovet.png")
-    lovetScale = 1
 end
 
 function love.draw()
-    -- only get the screen size frome love.draw callback
+    -- IMPORTANT: get the screen size from love.draw callback
     x, y, w, h = love.window.getSafeArea()
-    love.graphics.draw(lovet, x+w/2, y+h/2, lovetScale, 
-        nil, nil, lovet:getWidth()/2, lovet:getHeight()/2)
+    love.graphics.draw(lovet, x+w/2, y+h/2, nil, 
+        w*.9/lovet:getWidth(), nil, lovet:getWidth()/2, lovet:getHeight()/2)
 end
 
 function love.update( dt )
-    -- operate over game state
-    lovetScale = lovetScale + 1*dt
+   -- operate over game state 
 end
